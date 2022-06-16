5a29d7b5ab7289b00b5bbeadeb01e5e9812d2d52
chrisw889 <chrisjworth1@gmail.com>
2022-06-16 17:29:18 +0200

    Build worlds function

---
+++ src/models/cluedoClasses.py
@@ -0,0 +1,32 @@
+class SolAtom():
+    def __init__(self, type, number):
+        super().__init__()
+        self.type = type
+        self.number = number
+
+    def __str__(self):
+        return str(self.type) + str(self.number)
+    
+    def equalTo(self, type, number):
+        return self.type == type and self.number == number
+
+class AgentCard(SolAtom):
+    def __init__(self, type, number, agent):
+        super().__init__(type, number)
+        self.agent = agent
+    
+    def __str__(self):
+        return str(self.agent) + str(super())
+        
+    def equalTo(self, type, number, agent):
+        return self.type == type and self.number == number and self.agent == agent
+
+class CluedoWorld():
+    def __init__(self, atoms):
+        self.atoms = atoms
+    
+    def __str__(self):
+        name = ''
+        for atom in self.atoms:
+            name += str(atom)
+        return name
--- src/models/logic_model.py
+++ src/models/logic_model.py
@@ -1,30 +1,91 @@
+from bisect import insort
 from mlsolver.kripke import *
 from mlsolver.formula import *
+from cluedoClasses import SolAtom, AgentCard, CluedoWorld
 
-from agent_model import Player
+def buildWorlds(weapons, people, rooms, agents, type, nextAgent, dealt):
+  print(type, nextAgent, dealt)
 
-agents = ['a','b']
-n_weapons = 3
-n_people = 3
-n_rooms = 3
+  if len(dealt) == len(weapons) + len(people) + len(rooms):
+    assignment = {}
+    for i_atom in range(0, len(dealt)):
+      assignment[str(dealt[i_atom])] = True;
+    return [World(str(CluedoWorld(dealt)), assignment)]
 
-worlds = []
-for i_weapon in range(0,n_weapons):
-  for i_person in range(0,n_people):
-    for i_room in range(0,n_rooms):
-      worlds.append(World(
-        ("("+str(i_weapon)+","+str(i_person)+","+str(i_room)+")"),
-        {("w"+str(i_weapon)): True, ("p"+str(i_person)): True, ("r"+str(i_room)): True})
-      )
+  worlds = []
 
-relations = {('(0,0,0)', '(0,0,0)')}
+  if type == 'w':
+    
+    if nextAgent < 0:
+      for weapon in weapons:
+        dealt.append(SolAtom('w', weapon))
+        worlds += buildWorlds(weapons, people, rooms, agents, 'p', -1, dealt)
+        dealt.pop()
+      return worlds
 
-agent = Player(1, 75)
-agent.setAtributes(2, 7, 5)
-agent.weapon = 5
-agent.askPlayer(1, "weapon")
+    else:
+      for weapon in weapons:
+        weapons.remove(weapon)
+        dealt.append(AgentCard('w', weapon, nextAgent))
+        worlds += buildWorlds(weapons, people, rooms, agents, 'p', (nextAgent + 1) % len(agents), dealt)
+        insort(weapons, weapon)
+      return worlds
 
-print(agent.weapon)
+  if type == 'p':
 
-#ks = KripkeStructure(worlds, relations)
-#print(ks.solve(And(And(Atom('w1'),Atom('p1')),Atom('r1'))))
+    if nextAgent < 0:
+      for person in people:
+        dealt.append(SolAtom('p', person))
+        worlds += buildWorlds(weapons, people, rooms, agents, 'r', -1, dealt)
+        dealt.pop()
+      return worlds
+
+    else:
+      for person in people:
+        people.remove(person)
+        dealt.append(AgentCard('p', person, nextAgent))
+        worlds += buildWorlds(weapons, people, rooms, agents, 'r', (nextAgent + 1) % len(agents), dealt)
+        insort(people, person)
+      return worlds
+      
+  if type == 'r':
+
+    if nextAgent < 0:
+      for room in rooms:
+        dealt.append(SolAtom('r', room))
+        worlds += buildWorlds(weapons, people, rooms, agents, 'w', 0, dealt)
+        dealt.pop()
+      return worlds
+
+    else:
+      for room in rooms:
+        rooms.remove(room)
+        dealt.append(AgentCard('r', room, nextAgent))
+        worlds += buildWorlds(weapons, people, rooms, agents, 'w', (nextAgent + 1) % len(agents), dealt)
+        insort(rooms, room)
+      return worlds
+
+# from agent_model import Player
+
+n_agents = 3
+agents = ['a','b','c']
+n_weapons = 2
+weapons = list(range(0, n_weapons))
+n_people = 2
+people = list(range(0, n_people))
+n_rooms = 2
+rooms = list(range(0, n_rooms))
+
+worlds = buildWorlds(weapons, people, rooms, agents, 'w', -1, [])
+print(worlds)
+
+# agent = Player(1, 75)
+# agent.setAtributes(2, 7, 5)
+# agent.weapon = 5
+# agent.askPlayer(1, "weapon")
+
+# print(agent.weapon)
+
+# ks = KripkeStructure(worlds, relations)
+# print(len(ks.worlds))
+# print(ks.solve(And(And(Atom('w1'),Atom('p1')),Atom('r1'))))
